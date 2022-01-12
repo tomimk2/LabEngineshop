36957d5d5f58f027dabb5652039aae19b6033b41
Tomas Alvarez <alvareztomas2408@gmail.com>
2022-01-12 12:58:24 -0300

    navbar

---
+++ src/components/navbar.js
@@ -0,0 +1,20 @@
+import './navbar.css';
+
+
+function Navbar() {
+return (
+   
+       <nav>
+           <ul className="Nav">
+               <li><a class="menu" href="">Inicio</a></li>
+               <li><a class="menu" href="">Categorias</a></li>
+               <li><a class="menu" href="">About Us</a></li>
+               <li><a class="menu" href="">Contacto</a></li>
+           </ul>
+
+
+
+       </nav>);
+}
+
+export default Navbar;
